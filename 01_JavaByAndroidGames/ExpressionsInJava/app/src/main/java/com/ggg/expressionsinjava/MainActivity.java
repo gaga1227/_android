8161b1bb920fabbe1b0ebc3345dc430ff235b999
Johnny Xu <gaga1227@gmail.com>
2015-09-23 21:44:36 +1000

    chap3 - expressions and logs

---
+++ 01_JavaByAndroidGames/ExpressionsInJava/app/src/main/java/com/ggg/expressionsinjava/MainActivity.java
@@ -0,0 +1,37 @@
+package com.ggg.expressionsinjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //first we declare and initialize a few variables
+        int a = 10;
+        String b = "Alan Turing";
+        boolean c = true;
+
+        //Let's look at how Android 'sees' these variables
+        //by outputting them, one at a time to the console
+        Log.i("info", "a = " + a);
+        Log.i("info", "b = " + b);
+        Log.i("info", "c = " + c);
+
+        //Now let's make some changes
+        a++;
+        a = a + 10;
+        b = b + " was smarter than the average bear Booboo";
+        b = b + a;
+        c = (1 + 1 == 3); //1 + 1 is definitely 2! So false.
+
+        //Now to output them all again
+        Log.i("info", "a = " + a);
+        Log.i("info", "b = " + b);
+        Log.i("info", "c = " + c);
+    }
+}
