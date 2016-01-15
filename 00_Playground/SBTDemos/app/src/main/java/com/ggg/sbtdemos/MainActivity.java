b397d0622f18dc27e458526a6d583ce25371a78f
Johnny Xu <gaga1227@gmail.com>
2016-01-15 22:44:00 +1100

    Playground - SBTDemos - added webview

--- 00_Playground/SBTDemos/app/src/main/java/com/ggg/sbtdemos/MainActivity.java
+++ 00_Playground/SBTDemos/app/src/main/java/com/ggg/sbtdemos/MainActivity.java
@@ -2,7 +2,9 @@ package com.ggg.sbtdemos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +13,23 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webview = (WebView)findViewById(R.id.webView);
+        // get webview
+        WebView webview = (WebView) findViewById(R.id.webView);
+
+        // tweak settings
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webview.getSettings().setAllowFileAccessFromFileURLs(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.getSettings().setUseWideViewPort(false);
+        if ((android.os.Build.VERSION.SDK_INT) >= 23) {
+            webview.getSettings().setOffscreenPreRaster(true);
+        }
+        if ((android.os.Build.VERSION.SDK_INT) >= 21) {
+            webview.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        // loading webview URL
         webview.loadUrl("https://m.sportsbet.com.au");
     }
 }
