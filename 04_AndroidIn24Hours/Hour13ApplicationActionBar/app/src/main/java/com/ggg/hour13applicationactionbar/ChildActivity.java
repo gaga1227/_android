bacd26703d5ebb99e6a20ade9c33ac89f2e9aa63
Johnny Xu <gaga1227@gmail.com>
2016-01-29 20:12:46 +1100

    AndroidIn24 - hour13 - completed action bar example

--- 04_AndroidIn24Hours/Hour13ApplicationActionBar/app/src/main/java/com/ggg/hour13applicationactionbar/ChildActivity.java
+++ 04_AndroidIn24Hours/Hour13ApplicationActionBar/app/src/main/java/com/ggg/hour13applicationactionbar/ChildActivity.java
@@ -2,12 +2,23 @@ package com.ggg.hour13applicationactionbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
 
+    /**
+     * onCreate
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+
+        // set content title
+        String title = getIntent().getExtras().getString("CHILD_CONTENT_TITLE");
+        TextView titleText = (TextView) findViewById(R.id.titleText);
+        titleText.setText(title);
+        setTitle("Child: " + title);
     }
 }
