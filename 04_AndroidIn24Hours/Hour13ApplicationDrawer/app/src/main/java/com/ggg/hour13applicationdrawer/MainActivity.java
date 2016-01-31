3e441a37dddb987b13a24c4b5f3b229b87800a08
Johnny Xu <gaga1227@gmail.com>
2016-01-31 21:12:01 +1100

    AndroidIn24 - hour13 - cleaned up drawer example

--- 04_AndroidIn24Hours/Hour13ApplicationDrawer/app/src/main/java/com/ggg/hour13applicationdrawer/MainActivity.java
+++ 04_AndroidIn24Hours/Hour13ApplicationDrawer/app/src/main/java/com/ggg/hour13applicationdrawer/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
 
     // drawer toggle
-    private ActionBarDrawerToggle drawerLeftToggle;
+    private ActionBarDrawerToggle drawerToggle;
 
     // menu list views
     private ListView drawerLeftListView;
@@ -39,11 +39,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // init
+        _initToolbar();
+        _initDefaultDrawer();
+        _initRightDrawer();
+        _initDrawerMenus();
+    }
 
-        /**
-         * Toolbar
-         */
 
+    /**
+     * onPostCreate
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Synchronize the state of the drawer indicator/affordance with the linked DrawerLayout
+        // This should be called from your Activity's onPostCreate method
+        drawerToggle.syncState();
+    }
+
+
+    /**
+     * Toolbar
+     */
+    private void _initToolbar() {
         // setup action bar using toolbar view
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -55,12 +75,13 @@ public class MainActivity extends AppCompatActivity {
         catch (NullPointerException exp) {
             Log.e("Error", exp.getMessage());
         }
+    }
 
 
-        /**
-         * Drawer Left (default with DrawerToggle)
-         */
-
+    /**
+     * Drawer Left (default with DrawerToggle)
+     */
+    private void _initDefaultDrawer() {
         drawerLeftListView = (ListView) findViewById(R.id.left_drawer);
         drawerRightListView = (ListView) findViewById(R.id.right_drawer);
 
@@ -69,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
         // create DrawerToggle
         // Notes: ActionBarDrawerToggle will ONLY handle a LEFT sided Drawer
-        drawerLeftToggle = new ActionBarDrawerToggle(
+        drawerToggle = new ActionBarDrawerToggle(
                 this,
                 drawerLayout,
                 toolbar,
@@ -81,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
 
+                // make sure only one of the drawers is open at any time
                 if (drawerView.equals(drawerLeftListView)) {
                     drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, drawerRightListView);
                 }
@@ -94,6 +116,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
 
+                // make sure only one of the drawers is open at any time
                 if (drawerView.equals(drawerLeftListView)) {
                     drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerRightListView);
                 }
@@ -101,23 +124,31 @@ public class MainActivity extends AppCompatActivity {
                     drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, drawerLeftListView);
                 }
             }
+
+            // Called when a drawer's position changes. e.g. during sliding
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
         };
 
         // setup DrawerToggle with DrawerLayout
-        drawerLayout.setDrawerListener(drawerLeftToggle);
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        // TODO: 31/01/2016 Fix right drawer updates home indicator issue
+    }
 
 
-
-        /**
-         * Drawer Right (handled manually via a button view)
-         */
-
-        // TODO: fix bug of right drawer updates indicator
-
+    /**
+     * Drawer Right (handled manually via a button view)
+     */
+    private void _initRightDrawer() {
+        // using TextView as right drawer toggle button
         TextView rightDrawerToggle = (TextView) findViewById(R.id.rightDrawerToggle);
         rightDrawerToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // toggle logic on right drawer
                 if (drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
                     drawerLayout.closeDrawer(Gravity.RIGHT);
                 } else {
@@ -125,24 +156,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-
-        /**
-         * Drawers Menus
-         */
-
-        // TODO: adding menu items
-
     }
 
 
     /**
-     * onPostCreate
+     * Drawers Menus
      */
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        drawerLeftToggle.syncState();
+    private void _initDrawerMenus() {
+        // TODO: 31/01/2016 adding menu items and updates dummy content
     }
 }
