f57ec83c238f6e9d4db0e8575aafc6801be76a8f
Johnny Xu <gaga1227@gmail.com>
2017-04-28 22:57:25 +1000

    kotlinInAction - chap1 - completed

---
+++ 05_KotlinInAction/chaptors-2016_v11/src/ch01/ATasteOfKotlin/01_ATasteOfKotlin.kt
@@ -0,0 +1,16 @@
+package ch01.ATasteOfKotlin
+
+data class Person(
+		val name: String,
+		val age: Int? = null) //nullable type (Int?); default value for argument
+
+fun main(args: Array<String>) {
+	val persons = listOf(
+			Person("Alice"),
+			Person("Bob", age = 39)) //named argument
+	val oldestPerson = persons.maxBy { it.age ?: 0 } //lambda expression; "elvis" operator
+	println("The oldest is: $oldestPerson") //string template
+
+	// The oldest is: Person(name=Bob, age=39) //auto generated 'toString'
+}
+
