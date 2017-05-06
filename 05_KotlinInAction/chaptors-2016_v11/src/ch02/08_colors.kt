4c926434340bfd28147c80c17b9dfa3cd4f8f8c8
Johnny Xu <gaga1227@gmail.com>
2017-05-06 23:08:48 +1000

    kotlinInAction - chap4 - started

--- 05_KotlinInAction/chaptors-2016_v11/src/ch02/08_colors.kt
+++ 05_KotlinInAction/chaptors-2016_v11/src/ch02/08_colors.kt
@@ -4,8 +4,21 @@ enum class Color {
 	RED, ORANGE, YELLOW, GREEN, BLUE, INDIGO, VIOLET
 }
 
+enum class SymbolicColor(val symbol: String) {
+	RED("Passion"),
+	ORANGE("Kind"),
+	YELLOW("Warmth"),
+	GREEN("Peace"),
+	BLUE("Space"),
+	INDIGO("Excitement"),
+	VIOLET("Mystery")
+}
+
 fun main(args: Array<String>) {
 	for (color in Color.values()) {
 		println(color)
+	}
+	for (symbolicColor in SymbolicColor.values()) {
+		println("${symbolicColor.name} Symbols ${symbolicColor.symbol}")
 	}
 }
---
+++ 05_KotlinInAction/chaptors-2016_v11/src/ch04/01_InterfacesInKotlinMethodsWithDefaultImplementations.kt
@@ -0,0 +1,61 @@
+package ch04
+
+interface Clickable {
+	fun click()
+	// has function body, as default methods in Java 8
+	fun sameNameSignature() = println("I'm clickable! from Clickable")
+	fun sameNameDifferentSignature(type: String) = println("I'm clickable! from Clickable $type")
+}
+
+interface Focusable {
+	// has function body, as default methods in Java 8
+	fun setFocus(focused: Boolean) = println("I ${if (focused) "got" else "lost"} focus.")
+	fun sameNameSignature() = println("I'm focusable! from Focusable")
+	fun sameNameDifferentSignature(type: Int) = println("I'm clickable! from Focusable $type")
+}
+
+abstract class AbstractButton : Focusable, Clickable {
+	// abstract class only needs to implement same method from multiple interfaces
+	override fun sameNameSignature() {
+		// invoke super methods
+		super<Clickable>.sameNameSignature() // same as 'Clickable.super.sameNameSignature() in Java'
+		super<Focusable>.sameNameSignature() // same as 'Focusable.super.sameNameSignature() in Java'
+	}
+}
+
+// colon ':' as 'implements' in Java
+class Button : Focusable, Clickable {
+	// use 'override' modifier to implement a method
+	override fun click() = println("I was clicked! from Button class implementation")
+
+	// must implement same method from multiple interfaces
+	override fun sameNameSignature() {
+		println("Super methods are overridden here")
+	}
+}
+
+// colon ':' as 'extends' in Java
+// abstract class must be initialised here
+class Button2 : AbstractButton() {
+	// only needs to implement body-less method from interface
+	override fun click() = println("I was clicked! from AbstractButton subclass implementation")
+}
+
+fun main(args: Array<String>) {
+	val button1 = Button()
+	button1.click() // calls implementation
+	button1.setFocus(true) // calls default implementation in interface
+	button1.sameNameSignature() // calls implementation in class
+	button1.sameNameDifferentSignature("called from button1"); // calls default implementation in interface
+	button1.sameNameDifferentSignature(1); // calls default implementation in interface
+
+	println("----------------------------------------------")
+
+	val button2 = Button2()
+	button2.click() // calls subclass implementation
+	button2.setFocus(false) // calls default implementation in interface
+	button2.sameNameSignature() // calls implementation in parent class
+	button2.sameNameDifferentSignature("called from button2"); // calls default implementation in interface
+	button2.sameNameDifferentSignature(2); // calls default implementation in interface
+}
+
