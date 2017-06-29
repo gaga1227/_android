e2dc7414ff3fb347ced3e3fadd05c77b5ee27a8d
Johnny Xu <gaga1227@gmail.com>
2017-06-29 21:16:13 +1000

    kotlinInAction - chap5 - wip

--- 05_KotlinInAction/chaptors-2016_v11/src/ch05/27_PassingLambdaAsParameterToJavaMethod.kt
+++ 05_KotlinInAction/chaptors-2016_v11/src/ch05/27_PassingLambdaAsParameterToJavaMethod.kt
@@ -3,9 +3,34 @@ package ch05
 import ch05.Java.ClassWithMethodTakesFunctionalInterfaceParam
 
 fun main(args: Array<String>) {
-	// in Java, functional interface, or SAM interface
-	// is interface has only one abstract method hence SAM (Single Abstract Method)
-	// You can pass a lambda to any Java method that expects a functional interface
 	val javaClass = ClassWithMethodTakesFunctionalInterfaceParam()
-	javaClass.runWithDelay(1000) { println("one second's up!") }
+	var delay: Long
+
+	// in Java, functional interface, or SAM interface
+	// is interface has only one abstract method, hence SAM (Single Abstract Method)
+	// You can pass a lambda to any Java method that expects a functional interface
+	delay = 1000L
+	javaClass.runWithDelay(delay) { println("one second's up!") }
+
+	// Using lambda without access to variable from its context like above is equivalent to
+	// creating only one instance to a static variable
+	// the single instance of the anonymous class that implementing SAM is reused
+	delay = 2000L
+	val runnable = Runnable { println("two seconds are up!") }
+	javaClass.runWithDelay(delay, runnable)
+
+	// Using lambda with access to variable from its context is equivalent to
+	// creating different instances of a class for each time
+	delay = 3000L
+	javaClass.runWithDelay(delay) { println("${delay / 1000} seconds delayed!") }
+	delay = 4000L
+	javaClass.runWithDelay(delay, SomeRunnable(delay))
+}
+
+// Some runnable class kotlin compiles automatically
+// for generating instances from lambdas
+class SomeRunnable(val delay: Long) : Runnable {
+	override fun run() {
+		println("${delay / 1000} seconds delayed!")
+	}
 }
--- 05_KotlinInAction/chaptors-2016_v11/src/ch05/27_SAMConstructorsExplicitConversionOfLambdasToFunctionalInterfaces.kt
+++ 05_KotlinInAction/chaptors-2016_v11/src/ch05/27_SAMConstructorsExplicitConversionOfLambdasToFunctionalInterfaces.kt
@@ -1,3 +1,33 @@
 package ch05
 
 
+fun main(args: Array<String>) {
+	// you cannot return a lambda directly
+	// but can wrap it inside a SAM constructor
+	// that returns an instance of the functional interface
+	fun createSAMInstanceFromLambda(name: String): Runnable {
+		// The name of the SAM constructor is the same as
+		// the name of the underlying functional interface.
+		// The SAM constructor takes a single lambda argument that will
+		// be used as the body of the single abstract method in the functional interface
+		// and returns an instance of the class implementing the interface.
+		return Runnable { println("Your runnable: '${name}' is here!") }
+	}
+
+	// creates a runnable instance and run it
+	createSAMInstanceFromLambda("John").run()
+}
+
+// Some other example in Android
+/*
+val listener = OnClickListener { view ->
+	val text = when (view.id) {
+		R.id.button1 -> "First button"
+		R.id.button2 -> "Second button"
+		else -> "Unknown button"
+	}
+	toast(text)
+}
+button1.setOnClickListener(listener)
+button2.setOnClickListener(listener)
+*/
